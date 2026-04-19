1ef41b2ac267019d745792021cbb0ceaa4d1895f
Satvik Singh <satviksingh28@gmail.com>
2026-04-19 23:22:36 +0530

    Two migrations

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String 
+from sqlalchemy import Column, Integer, String, ForeignKey, Numeric
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -8,3 +8,11 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
+    email = Column(String(100))
+
+class Salary(Base):
+    __tablename__ = "salary"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    amount = Column(Numeric(10, 2))
